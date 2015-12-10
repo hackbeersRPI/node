183307944e39ddba9d273775062ed3f8ac52337d
Marian C Moldovan <moldovan0991@gmail.com>
2015-12-10 10:23:44 +0100

    Changed structure

--- utils/install-nodered.sh
+++ utils/install-nodered.sh
@@ -7,3 +7,9 @@ sudo apt-get install -y build-essential python-dev python-rpi.gpio nodejs
 # Installing nodered
 sudo npm cache clean
 sudo npm install -g --unsafe-perm  node-red
+
+sudo npm node-red-contrib-advanced-ping -g --unsafe-perm
+sudo npm node-red-contrib-ds18b20-sensor -g --unsafe-perm
+sudo npm node-red-contrib-eddystone -g --unsafe-perm
+sudo npm node-red-node-physical-web -g --unsafe-perm
+sudo npm node-red-node-sqlite -g --unsafe-perm
